361d3c43462154dc44675504fd1de23114492fd4
Matheus Alves Dias <149620421+MatheusAlvesDias@users.noreply.github.com>
2023-11-15 14:51:08 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import { Link, NavLink } from "react-router-dom"
-import { SiPokemon } from 'react-icons/si'
 import './Navbar.css'
 import { BsSearch } from 'react-icons/bs'
 import './SearchForm.css'
