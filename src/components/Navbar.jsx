e3b2af67d7ef3b9e81fadd33073119d5cae30574
Matheus Alves Dias <149620421+MatheusAlvesDias@users.noreply.github.com>
2023-11-15 14:42:32 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({search, setSearch, findPokemon}) => {
 
         <nav className="navbar">
             <div>
-                <img src="../../public/pokelogo.png" height='100' width='200' alt="pokemon.logo" />
+                <img src="public/pokelogo.png" height='100' width='200' alt="pokemon.logo" />
             </div>
             <div className='search-form'>
         <form onSubmit={findPokemon}>
